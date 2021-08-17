0f25bcf824899660235a4589b87acca27af64eb6
Tom Gurion <nagasaki45@gmail.com>
2021-08-17 10:54:55 +0100

    Correct SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.shevet-ahim.co.il'
+SITEURL = 'https://shevet-ahim.co.il'
 
 DELETE_OUTPUT_DIRECTORY = True
 
